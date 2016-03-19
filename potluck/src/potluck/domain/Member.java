9b5df1c36d98bf90cb9fce2e71644492d3241be8
Jessie Wang <chywang.tju@gmail.com>
2016-03-18 22:00:35 -0400

    change member and member builder

--- potluck/src/potluck/domain/Member.java
+++ potluck/src/potluck/domain/Member.java
@@ -5,11 +5,74 @@ import java.util.ArrayList;
 public class Member {
 	private String password;
 	private String userName;
+	private int memberID;
+	private boolean isAdmin;
+	private String alias;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
 	private ArrayList<Recipe> recipeList;
-	public Member(String userName, String password){
+	
+	public Member(){
 		recipeList=new ArrayList<>();
-		this.userName=userName;
-		this.password=password;
+		
+		
+	}
+	public int getMemberID() {
+		return memberID;
+	}
+	public void setMemberID(int memberID) {
+		this.memberID = memberID;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public ArrayList<Recipe> getRecipeList() {
+		return recipeList;
+	}
+	public void setRecipeList(ArrayList<Recipe> recipeList) {
+		this.recipeList = recipeList;
+	}
+	public Member(MemberBuilder memberBuilder) {
+		recipeList=new ArrayList<>();
+		// TODO Auto-generated constructor stub
+		this.password=memberBuilder.getPassword();
+		this.userName=memberBuilder.getUserName();
+		this.alias=memberBuilder.getAlias();
+		this.memberID=memberBuilder.getMemberID();
+		this.isAdmin=memberBuilder.isAdmin();
+		this.firstName=memberBuilder.getFirstName();
+		this.lastName=memberBuilder.getLastName();
+		this.email=memberBuilder.getEmail();
 	}
 	public String getPassword() {
 		return password;
---
+++ potluck/src/potluck/domain/MemberBuilder.java
@@ -0,0 +1,99 @@
+package potluck.domain;
+
+import java.util.ArrayList;
+
+public class MemberBuilder {
+	private String password;
+	private String userName;
+	private int memberID;
+	private boolean isAdmin;
+	private String alias;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	private MemberBuilder() {
+	}
+
+	public static MemberBuilder create() {
+		MemberBuilder mb = new MemberBuilder();
+		return mb;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public int getMemberID() {
+		return memberID;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public MemberBuilder password(String password) {
+		this.password = password;
+		return this;
+	}
+
+	public MemberBuilder username(String username) {
+		this.userName = username;
+		return this;
+	}
+
+	public MemberBuilder alias(String alias) {
+		this.alias = alias;
+		return this;
+	}
+
+	public MemberBuilder memberID(int memberID) {
+		this.memberID = memberID;
+		return this;
+	}
+
+	public MemberBuilder firstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
+	public MemberBuilder lastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public MemberBuilder email(String email) {
+		this.email = email;
+		return this;
+	}
+	public MemberBuilder admin(boolean admin){
+		this.isAdmin=admin;
+		return this;
+	}
+	
+
+	public Member build() {
+		return new Member(this);
+	}
+
+}
